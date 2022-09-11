8752661818236e5da0860ee92cfd6fcce9edd726
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2022-09-11 09:02:58 -0300

    ruteo

--- finalprogramacion/src/App.js
+++ finalprogramacion/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react'
-//import {Route, Switch} from 'react-router-dom'
+import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Nav from './components/Nav/Nav';
+import Home from './pages/Home/Home'
+import Favs from './pages/Favs/Favs'
+//import DetallePeli from './pages/DetallePeli/DetallePeli'
+import NotFound from './pages/NotFound/NotFound'
 
 
 function App() {
   return (
     <>
     <Nav/>
+    <Switch>
+          <Route path="/" exact  >
+            <Home />
+          </Route>
+          <Route path="/favs" component={Favs} />
+          {/* <Route path="/detallepelicula/id/:id" component={DetallePeli} /> */}
+          <Route component={NotFound}/>
+        </Switch>
     <Footer/> 
     </>
   );
