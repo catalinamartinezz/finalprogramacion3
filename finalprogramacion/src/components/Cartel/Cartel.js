36e5e11cea13e4adc86b6c7b816bbc92d566fc2a
luisigarciia <garcial@udesa.edu.ar>
2022-09-13 11:16:53 -0300

    detalle cartelera

--- finalprogramacion/src/components/Cartel/Cartel.js
+++ finalprogramacion/src/components/Cartel/Cartel.js
@@ -1,30 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Cartel.css'
+import { Link } from 'react-router-dom'
 
-function Cartel ({cartel}){
-    console.log(cartel)
-    const img= 'https://image.tmdb.org/t/p/w342';
-    let {poster_path, title, overview}= cartel
-return(
-
-    <>
-             <article className="art-cartelera"> 
-                <a href="#?">
-                    <img className= "img-home" src={img + poster_path} alt={title}/> 
-                </a>
-            <h2 className="titulos-inicio-cartelera" >{title}</h2>
-        <p>Descripción: {overview}</p>
+export default class Cartel extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+        }
+      }  
+    render() {
+        const img= 'https://image.tmdb.org/t/p/w342';
+    return (
+      <>
+       <article className="art-cartelera"> 
+       <Link to={`/detallepelicula/id/${this.props.cartel.id}`}>
+            <img src={`${img + this.props.cartel.poster_path}`}  className= "img-home" alt=""/>
+        </Link>
+            <h2 className="titulos-inicio-cartelera" >{this.props.cartel.title}</h2>
+        <p>Descripción: {this.props.cartel.overview}</p>
         <div className='d-flex justify-content-end'>
-        <button className="btn btn-primary"  >Favoritos</button>
+        <button className="btn btn-primary" onClick={()=>this.props.favoritos(this.props.cartel)} >Favoritos</button> {/* this.state.esFavorito? <p>Quitar</> : <p>agregarFavorito</p>*/}
         <button className="btn btn-danger"  >Borrar</button>
         <button className="btn btn-info" >Ver Mas</button>
         
       </div>
     </article>
-
-    </>
-
-)
+      </>
+    )
+  }
 }
-
-export default Cartel
