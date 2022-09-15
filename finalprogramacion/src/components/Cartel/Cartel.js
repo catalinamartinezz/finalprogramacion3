a9ad651069b646eb6511067714072523ede5120b
unknown <jvillanueva@udesa.edu.ar>
2022-09-15 10:30:01 -0300

    ver mas cartel

--- finalprogramacion/src/components/Cartel/Cartel.js
+++ finalprogramacion/src/components/Cartel/Cartel.js
@@ -6,8 +6,12 @@ export default class Cartel extends Component {
     constructor(props){
         super(props)
         this.state = {
+          descripcion: false,
         }
       }  
+    vermas= ()=> {
+      this.setState({descripcion: !this.state.descripcion})
+    }
     render() {
         const img= 'https://image.tmdb.org/t/p/w342';
     return (
@@ -16,13 +20,12 @@ export default class Cartel extends Component {
        <Link to={`/detallepelicula/id/${this.props.cartel.id}`}>
             <img src={`${img + this.props.cartel.poster_path}`}  className= "img-home" alt=""/>
         </Link>
-            <h2 className="titulos-inicio-cartelera" >{this.props.cartel.title}</h2>
-        <p>Descripción: {this.props.cartel.overview}</p>
+      <h2 className="titulos-inicio-cartelera" >{this.props.cartel.title}</h2>
         <div className='d-flex justify-content-end'>
         <button className="btn btn-primary" onClick={()=>this.props.favoritos(this.props.cartel)} >Favoritos</button> {/* this.state.esFavorito? <p>Quitar</> : <p>agregarFavorito</p>*/}
-        <button className="btn btn-info" >Ver Mas</button>
-        
-      </div>
+        </div>
+        <p onClick= {this.vermas}>Ver Mas</p>
+        {this.state.descripcion == false? <></>: <p>{this.props.cartel.overview}</p>}
     </article>
       </>
     )
