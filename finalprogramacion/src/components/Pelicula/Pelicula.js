ccb1c7e590135e0cdb338f3b9c5953f4d4e2cd17
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2022-09-15 10:16:39 -0300

    ver mas pelicula

--- finalprogramacion/src/components/Pelicula/Pelicula.js
+++ finalprogramacion/src/components/Pelicula/Pelicula.js
@@ -6,7 +6,11 @@ export default class Pelicula extends Component {
   constructor(props){
     super(props)
     this.state = {
+      descripcion: false,
     }
+  }
+  vermas=()=>{
+    this.setState({descripcion: !this.state.descripcion})
   }
   render() {
     const img = 'https://image.tmdb.org/t/p/w342';
@@ -17,11 +21,11 @@ export default class Pelicula extends Component {
              <img src={`${img + this.props.peliculas.poster_path}`}  className= "img-home" alt=""/> 
           </Link>
           <h2 className="titulos-inicio" >{this.props.peliculas.title}</h2>
-          <p>Descripción: {this.props.peliculas.overview}</p>
           <div className='d-flex justify-content-end'>
           <button className="btn btn-primary" onClick={()=>this.props.favoritos(this.props.peliculas)}>Favoritos </button> {/* this.state.esFavorito? <p>Quitar</> : <p>agregarFavorito</p>*/}
-          <button className="btn btn-info" >Ver Mas</button>
-        </div>
+          </div>
+          <p onClick={this.vermas}> Ver mas</p> 
+          {this.state.descripcion == false? <></>:  <p>{this.props.peliculas.overview}</p> }
       </article>
       </>
     )
