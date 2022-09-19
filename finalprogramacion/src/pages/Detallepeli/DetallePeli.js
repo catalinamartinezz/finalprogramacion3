25dcfbaab47e56a47f026b136fafbea7206c80d7
catalinamartinezz <catalinamartinez@udesa.edu.ar>
2022-09-19 15:01:12 -0300

    detalle peli

--- finalprogramacion/src/pages/Detallepeli/DetallePeli.js
+++ finalprogramacion/src/pages/Detallepeli/DetallePeli.js
@@ -9,7 +9,8 @@ export default class DetallePeli extends Component {
       detalle: {},
       genre: "",
       favoritos: [], 
-      agregar: []
+      agregar: [], 
+      cargando: true,
     };
   }
   componentDidMount() {
@@ -47,28 +48,36 @@ export default class DetallePeli extends Component {
   render() {
     const img = 'https://image.tmdb.org/t/p/w342';
     return (
+
+      <>
+      {this.state.cargando === false ? (
+            <p>Cargando</p>
+          ) : (
       <>
       <section className="bloque-pelicula">
+
         <section className="imagen-pelicula">
           <img className="portada-pelicula" src={img + this.state.detalle.poster_path} alt={this.state.detalle.title}/>
         </section>
+
         <section className="peli">
           <article>
             <h1 className="titulo-peli">{this.state.detalle.title}</h1>
-            <p>| Calificación: {this.state.detalle.vote_average} |
-             Duración: ${this.state.detalle.runtime} |
+            <p>
+              | Calificación: {this.state.detalle.vote_average} |
+               Duración: {this.state.detalle.runtime} |
                Genero: {this.state.genre} |
-               Fecha De Estreno : {this.state.detalle.release_date}</p>
-               <div className='d-flex justify-content-end'>
-          <button className="btn btn-primary" onClick={()=> this.handleagregar()} >{this.state.agregar ? 'Agregar de favoritos' : 'Quitar de favoritos'}</button> 
-          </div>
-        </article>
-  
-        <article className="bloque-sinopsis-peli">
+               Fecha De Estreno : {this.state.detalle.release_date}
+            </p>
+            <button className="btn btn-primary" onClick={()=> this.handleagregar()} >{this.state.agregar ? 'Agregar de favoritos' : 'Quitar de favoritos'}</button> 
+          </article>
+          <article className="bloque-sinopsis-peli">
             <p className="sinopsis"> {this.state.detalle.overview}</p>
-        </article>
+          </article>
         </section>
       </section>
+      </>)}
+      
     </>
     )
   }
